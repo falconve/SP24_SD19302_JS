4281cc92bc8646679dc41ba85c50dd93ca119fbe
falconve <falconve@gmail.com>
2024-06-24 08:41:19 +0700

    Bai 1/ Buoi1

---
+++ bai1.js
@@ -0,0 +1,17 @@
+// Hàm console.log để hiển thị ra kết quả
+// ở dưới f12 (tab console)
+console.log("Tuan Anh"); // Một chuỗi ký tự
+console.log(123); // Một số
+
+// Hàm alert để hiện thị kết quả
+//theo dạng pop-up
+//alert("js cơ bản");
+
+// Hàm document.write để hiển thị kết quả
+// theo dạng html
+document.write(123);
+
+// Hàm để nhập dữ liệu
+let input = prompt("Nhập điiiiiiii.........");
+// Sử dụng console.log để in dữ liệu vừa nhập vào
+console.log(input);
